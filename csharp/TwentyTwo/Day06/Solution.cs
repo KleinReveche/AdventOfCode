88504e7ee3c423d2f84a791f3c9d19c67dfa4cd8
Klein Reveche <therealklr2003@gmail.com>
2023-12-08 20:34:24 +0800

    [c#] 2022 Day 6: Tuning Problem

---
+++ csharp/TwentyTwo/Day06/Solution.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode.TwentyTwo.Day06;
+
+[Problem(Year.TwentyTwo, "Day06", "Tuning Trouble")]
+public class Solution(string input) : ISolution
+{
+    public object PartOne()
+    {
+        var startPacketIndex = ProcessData(4);
+        return $"The characters that were needed to be processed before the first start-of-pocket were {startPacketIndex}";
+    }
+
+    public object PartTwo()
+    {
+        var startMessageIndex = ProcessData(14);
+        return $"The characters that were needed to be processed before the first start-of-message were {startMessageIndex}\n";
+    }
+
+    private int ProcessData(int charNum)
+    {
+        var data = input.Trim();
+        var index = 0;
+
+        while (index + charNum < data.Length)
+        {
+            var subString = data.Substring(index, charNum);
+            if (subString.Distinct().Count() == charNum) return index + charNum;
+            index++;
+        }
+
+        return -1;
+    }
+}
