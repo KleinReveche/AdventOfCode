a14faa7069f3f0d73e4223b97bd707356b2c3018
Klein Reveche <therealklr2003@gmail.com>
2023-12-10 19:01:53 +0800

    [c#] 2023 Day 3: Gear Ratios

---
+++ csharp/TwentyThree/Day03/Solution.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.TwentyThree.Day03;
+
+[Problem(Year.TwentyThree, "Day03", "Gear Ratios")]
+public partial class Solution : ISolution
+{
+    private readonly string[] _data;
+    private readonly Dictionary<Symbol, HashSet<int>> _numbers;
+    private readonly List<Symbol> _symbols;
+
+    public Solution(string input)
+    {
+        _data = input.Trim().Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        _symbols = FindSymbols();
+        _numbers = GetNumbers();
+    }
+
+    public object PartOne()
+    {
+        var sum = _numbers.Sum(kvp => kvp.Value.Sum());
+        return $"The sum of all of the part numbers in the engine schematic is {sum}.";
+    }
+
+    public object PartTwo()
+    {
+        var sum = _numbers.Sum(it =>
+        {
+            if (it.Key.Sign == '*' && it.Value.Count == 2) return it.Value.Aggregate((a, b) => a * b);
+            return 0;
+        });
+        return $"the sum of all of the gear ratios in your engine schematic is {sum}.";
+    }
+
+    private Dictionary<Symbol, HashSet<int>> GetNumbers()
+    {
+        var numbers = new Dictionary<Symbol, HashSet<int>>();
+        var regex = SymbolRegex();
+
+        foreach (var symbol in _symbols)
+            for (var y = symbol.Y - 1; y <= symbol.Y + 1; y++)
+            for (var x = symbol.X - 1; x <= symbol.X + 1; x++)
+            {
+                if (x == symbol.X && y == symbol.Y) continue;
+                if (!int.TryParse(_data[y][x].ToString(), out _)) continue;
+
+                var left = x - 1;
+                while (left >= 0 && _data[y][left] != '.' && !regex.IsMatch(_data[y][left].ToString())) left--;
+
+                var right = x + 1;
+                while (right < _data[y].Length && _data[y][right] != '.' &&
+                       !regex.IsMatch(_data[y][right].ToString())) right++;
+
+                var number = int.Parse(_data[y].Substring(left + 1, right - left - 1));
+
+                if (!numbers.TryGetValue(symbol, out var set))
+                {
+                    set = new HashSet<int>();
+                    numbers.Add(symbol, set);
+                }
+
+                set.Add(number);
+            }
+
+        return numbers;
+    }
+
+    private List<Symbol> FindSymbols()
+    {
+        var symbols = new List<Symbol>();
+        var regex = SymbolRegex();
+
+        for (var y = 0; y < _data.Length; y++)
+        for (var x = 0; x < _data[y].Length; x++)
+        {
+            var symbol = regex.Match(_data[y][x].ToString());
+            if (symbol.Success) symbols.Add(new Symbol(symbol.Value[0], x, y));
+        }
+
+        return symbols;
+    }
+
+    [GeneratedRegex(@"[^.0-9]")]
+    private static partial Regex SymbolRegex();
+
+    private record Symbol(char Sign, int X, int Y);
+}
